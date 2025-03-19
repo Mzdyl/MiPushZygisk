85ed0dc7d6fc2c23ee47339389761d8f26d09eca
DogEZ <127666673+DdogezD@users.noreply.github.com>
2025-03-20 01:18:31 +0800

    Update hook.cpp

--- module/jni/hook.cpp
+++ module/jni/hook.cpp
@@ -1,86 +1,31 @@
 #include "hook.h"
 #include "logging.h"
 #include <string>
+#include <unordered_map>
 
 using namespace std;
 
 jstring (*orig_native_get)(JNIEnv *env, jclass clazz, jstring keyJ, jstring defJ);
 
+static unordered_map<string, string> spoofedProperties = {
+    {"ro.product.brand", "Xiaomi"},
+    {"ro.product.manufacturer", "Xiaomi"},
+    {"ro.miui.ui.version.name", "V130"},
+    {"ro.miui.ui.version.code", "13"},
+    {"ro.miui.version.code_time", "1658851200"},
+    {"ro.miui.internal.storage", "/sdcard/"},
+    {"ro.miui.region", "CN"},
+    {"ro.miui.cust_variant", "cn"},
+    {"ro.vendor.miui.region", "CN"}
+};
+
 jstring my_native_get(JNIEnv *env, jclass clazz, jstring keyJ, jstring defJ) {
-    const char *key = env->GetStringUTFChars(keyJ, nullptr);
-    const char *def = env->GetStringUTFChars(defJ, nullptr);
-
-    jstring hooked_result = nullptr;
-
-    // MIUI
-    if (strcmp(key, "ro.product.brand") == 0) { // ro.product.brand=Xiaomi
-        hooked_result = env->NewStringUTF("Xiaomi");
-    } else if (strcmp(key, "ro.product.manufacturer") == 0) { // ro.product.manufacturer=Xiaomi
-        hooked_result = env->NewStringUTF("Xiaomi");
-    } else if (strcmp(key, "ro.miui.ui.version.name") == 0) { // ro.miui.ui.version.name=V130
-        hooked_result = env->NewStringUTF("V130");
-    } else if (strcmp(key, "ro.miui.ui.version.code") == 0) { // ro.miui.ui.version.code=13
-        hooked_result = env->NewStringUTF("13");
-    } else if (strcmp(key, "ro.miui.version.code_time") == 0) { // ro.miui.version.code_time=1658851200
-        hooked_result = env->NewStringUTF("1658851200");
-    } else if (strcmp(key, "ro.miui.internal.storage") == 0) { // ro.miui.internal.storage=/sdcard/
-        hooked_result = env->NewStringUTF("/sdcard/");
-    } else if (strcmp(key, "ro.miui.region") == 0) { // ro.miui.region=CN
-        hooked_result = env->NewStringUTF("CN");
-    } else if (strcmp(key, "ro.miui.cust_variant") == 0) { // ro.miui.cust_variant=cn
-        hooked_result = env->NewStringUTF("cn");
-    } else if (strcmp(key, "ro.vendor.miui.region") == 0) { // ro.vendor.miui.region=CN
-        hooked_result = env->NewStringUTF("CN");
-}
-
-    env->ReleaseStringUTFChars(keyJ, key);
-    env->ReleaseStringUTFChars(defJ, def);
-
-    if (hooked_result != nullptr) {
-        return hooked_result;
-    } else {
-        return orig_native_get(env, clazz, keyJ, defJ);
+    const char *keyChars = env->GetStringUTFChars(keyJ, nullptr);
+    string keyStr(keyChars);
+    env->ReleaseStringUTFChars(keyJ, keyChars);
+    auto it = spoofedProperties.find(keyStr);
+    if (it != spoofedProperties.end()) {
+        return env->NewStringUTF(it->second.c_str());
     }
-}
-
-void hookBuild(JNIEnv *env) {
-    LOGD("hook Build\n");
-    jclass build_class = env->FindClass("android/os/Build");
-    jstring new_brand = env->NewStringUTF("Xiaomi");
-    jstring new_manufacturer = env->NewStringUTF("Xiaomi");
-
-    jfieldID brand_id = env->GetStaticFieldID(build_class, "BRAND", "Ljava/lang/String;");
-    if (brand_id != nullptr) {
-        env->SetStaticObjectField(build_class, brand_id, new_brand);
-    }
-
-    jfieldID manufacturer_id = env->GetStaticFieldID(build_class, "MANUFACTURER", "Ljava/lang/String;");
-    if (manufacturer_id != nullptr) {
-        env->SetStaticObjectField(build_class, manufacturer_id, new_manufacturer);
-    }
-
-    env->DeleteLocalRef(new_brand);
-    env->DeleteLocalRef(new_manufacturer);
-
-    LOGD("hook Build done");
-}
-
-void hookSystemProperties(JNIEnv *env, zygisk::Api *api) {
-    LOGD("hook SystemProperties\n");
-
-    JNINativeMethod targetHookMethods[] = {
-            {"native_get", "(Ljava/lang/String;Ljava/lang/String;)Ljava/lang/String;",
-             (void *) my_native_get},
-    };
-
-    api->hookJniNativeMethods(env, "android/os/SystemProperties", targetHookMethods, 1);
-
-    *(void **) &orig_native_get = targetHookMethods[0].fnPtr;
-
-    LOGD("hook SystemProperties done: %p\n", orig_native_get);
-}
-
-void Hook::hook() {
-    hookBuild(env);
-    hookSystemProperties(env, api);
+    return orig_native_get(env, clazz, keyJ, defJ);
 }
