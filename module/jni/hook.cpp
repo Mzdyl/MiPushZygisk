a11dd22bffc0cf4fc530e83141f56ad784848767
Mzdyl <25149539+Mzdyl@users.noreply.github.com>
2025-08-24 20:46:17 +0800

    Refactor: Use unordered_map for property hooking

--- module/jni/hook.cpp
+++ module/jni/hook.cpp
@@ -1,46 +1,35 @@
 #include "hook.h"
 #include "logging.h"
 #include <string>
+#include <unordered_map>
 
 using namespace std;
 
 jstring (*orig_native_get)(JNIEnv *env, jclass clazz, jstring keyJ, jstring defJ);
 
+static unordered_map<string, string> spoofedProperties = {
+    {"ro.product.brand", "Xiaomi"},
+    {"ro.product.manufacturer", "Xiaomi"},
+    {"ro.miui.ui.version.name", "V130"},
+    {"ro.miui.ui.version.code", "13"},
+    {"ro.miui.version.code_time", "1658851200"},
+    {"ro.miui.internal.storage", "/sdcard/"},
+    {"ro.miui.region", "CN"},
+    {"ro.miui.cust_variant", "cn"},
+    {"ro.vendor.miui.region", "CN"}
+};
+
 jstring my_native_get(JNIEnv *env, jclass clazz, jstring keyJ, jstring defJ) {
-    const char *key = env->GetStringUTFChars(keyJ, nullptr);
-    const char *def = env->GetStringUTFChars(defJ, nullptr);
+    const char *keyChars = env->GetStringUTFChars(keyJ, nullptr);
+    string keyStr(keyChars);
+    env->ReleaseStringUTFChars(keyJ, keyChars);
 
-    jstring hooked_result = nullptr;
-
-    // MIUI
-    if (strcmp(key, "ro.product.brand") == 0) { // ro.product.brand=Xiaomi
-        hooked_result = env->NewStringUTF("Xiaomi");
-    } else if (strcmp(key, "ro.product.manufacturer") == 0) { // ro.product.manufacturer=Xiaomi
-        hooked_result = env->NewStringUTF("Xiaomi");
-    } else if (strcmp(key, "ro.miui.ui.version.name") == 0) { // ro.miui.ui.version.name=V130
-        hooked_result = env->NewStringUTF("V130");
-    } else if (strcmp(key, "ro.miui.ui.version.code") == 0) { // ro.miui.ui.version.code=13
-        hooked_result = env->NewStringUTF("13");
-    } else if (strcmp(key, "ro.miui.version.code_time") == 0) { // ro.miui.version.code_time=1658851200
-        hooked_result = env->NewStringUTF("1658851200");
-    } else if (strcmp(key, "ro.miui.internal.storage") == 0) { // ro.miui.internal.storage=/sdcard/
-        hooked_result = env->NewStringUTF("/sdcard/");
-    } else if (strcmp(key, "ro.miui.region") == 0) { // ro.miui.region=CN
-        hooked_result = env->NewStringUTF("CN");
-    } else if (strcmp(key, "ro.miui.cust_variant") == 0) { // ro.miui.cust_variant=cn
-        hooked_result = env->NewStringUTF("cn");
-    } else if (strcmp(key, "ro.vendor.miui.region") == 0) { // ro.vendor.miui.region=CN
-        hooked_result = env->NewStringUTF("CN");
-}
-
-    env->ReleaseStringUTFChars(keyJ, key);
-    env->ReleaseStringUTFChars(defJ, def);
-
-    if (hooked_result != nullptr) {
-        return hooked_result;
-    } else {
-        return orig_native_get(env, clazz, keyJ, defJ);
+    auto it = spoofedProperties.find(keyStr);
+    if (it != spoofedProperties.end()) {
+        return env->NewStringUTF(it->second.c_str());
     }
+
+    return orig_native_get(env, clazz, keyJ, defJ);
 }
 
 void hookBuild(JNIEnv *env) {
