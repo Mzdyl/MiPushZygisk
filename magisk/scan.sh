3586d54b78e29de36de6fcacf678d8d72658c7dc
Mzdyl <25149539+Mzdyl@users.noreply.github.com>
2025-08-25 01:36:06 +0800

    333

--- magisk/scan.sh
+++ magisk/scan.sh
@@ -6,14 +6,26 @@ fi
 
 LOG_TAG="MiPushZygisk"
 WHITELIST_FILE="$MODDIR/package.list"
+BLACKLIST_FILE="$MODDIR/blacklist.conf"
 
 log -p i -t $LOG_TAG "Starting package scan..."
 
 > "$WHITELIST_FILE"
 
-for PKG in $(pm list packages -3 | cut -d ':' -f 2); do
-  if pm dump "$PKG" | grep -Eq "com.xiaomi.push.service.XMPushService|com.xiaomi.mipush.sdk.PushMessageHandler|com.xiaomi.mipush.sdk.MessageHandleService"; then
-    log -p i -t $LOG_TAG "Found MiPush SDK in: $PKG"
+pm list packages -3 | cut -d ':' -f 2 | while read -r PKG; do
+  if [ -s "$BLACKLIST_FILE" ] && grep -qw "$PKG" "$BLACKLIST_FILE"; then
+    log -p i -t $LOG_TAG "Package [$PKG] is in the blacklist, skipping."
+    continue
+  fi
+
+  APK_PATH=$(pm path "$PKG" | head -n 1 | cut -d ':' -f 2)
+
+  if [ -z "$APK_PATH" ]; then
+    continue
+  fi
+
+  if aapt d badging "$APK_PATH" 2>/dev/null | grep -qE 'service:[[:space:]]name=.*(XMPushService|PushMessageHandler|MessageHandleService)'; then
+    log -p i -t $LOG_TAG "Found MiPush SDK in: $PKG (Passed blacklist check)"
     echo "$PKG" >> "$WHITELIST_FILE"
   fi
 done
